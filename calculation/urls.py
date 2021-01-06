65851e0969d547552e7ea3268c1725497e04f46e
rafi khan <rafi@atlogys.com>
2021-01-06 18:47:06 +0530

    CSRF token Issue resolved

--- calculation/urls.py
+++ calculation/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from django.conf.urls import url
 from django.conf import settings
 from calculation import views
+from django.views.decorators.csrf import csrf_exempt
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'get_portfolio/$', views.GetPortfolioView.as_view(), name='get_portfolio'),
-    url(r'rerun_portfolio/$', views.RerunPortfolio.as_view(), name='rerun_portfolio'),
-    url(r'portfolio/$', views.PortfolioView.as_view(), name='portfolio'),
+    url(r'get_portfolio/$', csrf_exempt(views.GetPortfolioView.as_view()), name='get_portfolio'),
+    url(r'rerun_portfolio/$', csrf_exempt(views.RerunPortfolio.as_view()), name='rerun_portfolio'),
+    url(r'portfolio/$', csrf_exempt(views.PortfolioView.as_view()), name='portfolio'),
 
 ]
